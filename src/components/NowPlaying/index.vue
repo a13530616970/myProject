c861441daca457af117d51666d4e37d21b321f49
a13530616970 <1224094930@qq.com>
2019-07-02 14:23:19 +0800

    add detaiPage

--- src/components/NowPlaying/index.vue
+++ src/components/NowPlaying/index.vue
@@ -5,11 +5,11 @@
       <ul>
         <li class="pullDown">{{pullDownMsg}}</li>
         <li v-for="item in movieList" :key="item.id">
-          <div class="pic_show" @tap="handleToDetail">
-            <img :src="item.img | setWH('128.180')" />
+          <div class="pic_show" @tap="handleToDetail(item.id)">
+            <img :src="item.img | setWH()" />
           </div>
           <div class="info_list">
-            <h2>
+            <h2 @tap="handleToDetail(item.id)">
               {{item.nm}}
               <img v-if="item.version" src="@/assets/maxs.png" />
             </h2>
@@ -35,8 +35,7 @@ export default {
       movieList: [],
       pullDownMsg: "",
       isLoading: true,
-      prevCityId: -1,
-      
+      prevCityId: -1
     };
   },
   //当有keep alive 才有这个生命周期函数
@@ -80,8 +79,8 @@ export default {
     });
   },
   methods: {
-    handleToDetail() {
-      console.log("handleToTouchEnd");
+    handleToDetail(movieId) {
+      this.$router.push("/movie/detail/1/" + movieId);
     },
     handleToScroll(pos) {
       if (pos.y > 30) {
--- src/main.js
+++ src/main.js
@@ -6,15 +6,20 @@ import store from './stores'//省略了index.js
 import axios from 'axios'
 Vue.prototype.axios = axios;
 
-Vue.filter('setWH',(url,arg)=>{
-  return url.replace(/w\.h/,arg);
+Vue.filter('setWH', (url, arg) => {
+  if (!url)
+    return
+
+  return url.replace(/w\.h/, arg);
+
+
 });
 
 import Scroller from '@/components/Srcoller'
-Vue.component('Scroller',Scroller)
+Vue.component('Scroller', Scroller)
 
 import Loading from '@/components/Loading'
-Vue.component('Loading',Loading)
+Vue.component('Loading', Loading)
 
 Vue.config.productionTip = false
 
